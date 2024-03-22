ddb2d7a5b1804bf85326d10082e6db1f2bebb3ef
aj <amj1886@hotmail.com>
2024-03-22 19:21:08 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,69 @@
+
+// https://quote-garden.onrender.com/api/v3/quotes
+
+const formSubmit = document.getElementById('submit');
+const resetButton = document.getElementById("reset")
+
+resetButton.addEventListener('click', () => {
+    document.getElementById("output-space").innerHTML = "";
+})
+
+function getResponse(out_lim, genre_selected) {
+    // const response = await fetch(`https://quote-garden.onrender.com/api/v3/quotes?genre=${genre_selected}&limit=${out_lim}`);
+    // const body = await response.json();
+    // console.log(body);
+    if ( genre_selected != "all")
+    {
+        return fetch(`https://quote-garden.onrender.com/api/v3/quotes?genre=${genre_selected}&limit=${out_lim}`)
+        .then(resp => {
+            return resp.json()})
+        .catch((error) => {
+            console.log(error);
+            throw new Error(error);
+        })
+    }
+    else {
+        return fetch(`https://quote-garden.onrender.com/api/v3/quotes?limit=${out_lim}`)
+        .then(resp => {
+            return resp.json()})
+        .catch((error) => {
+            console.log(error);
+            throw new Error(error);
+        })
+    }
+}
+
+formSubmit.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const quoteGenre = document.getElementById('genre-selector');
+    const outputLimit = document.getElementById('num-quotes');
+
+    const apiResp = await getResponse(outputLimit.value, quoteGenre.value.toLowerCase());
+    // console.log(apiResp.data);
+    
+    for (let i=0; i < parseInt(outputLimit.value); i++ ) {
+        let quoteHolder = document.querySelector('#output-space > ul');
+        let lastQuote = document.querySelector('div#output-space > ul > li:last-of-type');
+        console.log(apiResp.data[i].quoteText);
+        if  ( lastQuote && lastQuote.getAttribute('id') ) {
+            let lastIdx = lastQuote.getAttribute('id');
+            let outputTemplate = `<li id=${parseInt(lastIdx)+1}>${apiResp.data[i].quoteText} </li><br>`
+            // console.log(apiResp.data);
+            quoteHolder.innerHTML += outputTemplate; 
+        }
+        else {
+            const initList = document.createElement("ul")
+            initList.className = "output-list"
+            document.getElementById("output-space").appendChild(initList);
+            let quoteHolder = document.querySelector('#output-space > ul');
+            let outputTemplate = `<li id=1>${apiResp.data[i].quoteText} </li><br>`;
+            quoteHolder.innerHTML += outputTemplate;
+
+        }
+    }
+    let newOutput = document.getElementById('output-space');
+
+});
+
+
+
